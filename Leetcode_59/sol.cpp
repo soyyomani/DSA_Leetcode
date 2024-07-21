b3264c19112fc4493a7ca14656f60be3d96d7283
Manikanta <134987333+soyyomani@users.noreply.github.com>
2024-07-22 01:22:26 +0530

    Update sol.cpp

--- Leetcode_59/sol.cpp
+++ Leetcode_59/sol.cpp
@@ -2,33 +2,42 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-    static vector<vector<int> > generateMatrix(int n) {
-        vector<vector<int> > res(n, vector<int>(n));
+    static vector<vector<int>>generateMatrix(int n) 
+{
+        vector<vector<int>> res(n, vector<int>(n));
         int left = 0, top = 0;
         int right = n - 1, bottom = n - 1;
         int val = 1;
-        while (top <= bottom && left <= right) {
-            for (int j = left; j <= right; j++) {
+        while (top<=bottom && left<=right) 
+        {
+            for (int j=left;j<=right;j++) 
+            {
                 res[top][j] = val;
                 val += 1;
             }
             top++;
-            for (int i = top; i <= bottom; i++) {
+            for (int i=top;i<=bottom;i++) 
+            {
                 res[i][right] = val;
                 val += 1;
             }
             right--;
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
+            if (top<=bottom) 
+            {
+                for (int j=right;j>=left;j--) 
+                {
                     res[bottom][j] = val;
                     val += 1;
                 }
                 bottom--;
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
+            if (left<=right) 
+            {
+                for (int i=bottom;i>=top;i--) 
+                {
                     res[i][left] = val;
                     val += 1;
                 }
